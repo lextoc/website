dc9240c45caebcce52e2d8d857c21d8f2396596e
lextoc <alexander.claes10@gmail.com>
2020-02-09 22:17:52 +0100

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsdoc/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  root: true,
+  plugins: ['prettier', 'eslint-plugin-import-order-alphabetical', 'jsdoc'],
+  parser: 'babel-eslint',
+  rules: {
+    'jsx-a11y/label-has-for': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'prettier/prettier': ['error'],
+    'max-len': 'off',
+    'import-order-alphabetical/order': 'error',
+    'react/prop-types': 0,
+    'jsdoc/require-description': 1,
+    'jsdoc/require-description-complete-sentence': 1,
+    'max-lines': ['warn', 200],
+    'max-lines-per-function': ['warn', 25],
+  },
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
