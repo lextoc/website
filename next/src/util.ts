fe4b2b970c6ab07bc0f23f8ac5068c5c6894c282
lextoc <alexander.claes10@gmail.com>
2020-03-27 14:37:31 +0100

    chore: update strapi url

--- next/src/util.ts
+++ next/src/util.ts
@@ -1,5 +1,5 @@
 export const getStrapiUrl = () => {
   return process.env.NODE_ENV === "production"
-    ? "https://strapi.alexanderclaes.be"
+    ? "http://strapi.alexanderclaes.be"
     : "http://localhost:1337";
 };
